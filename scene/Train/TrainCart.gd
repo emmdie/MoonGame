21fcab2f439a661b68fb6abf57b6bddee80befe6
emmdie <emmanuel.diehl@ifsr.de>
2022-08-15 11:58:32 +0200

    adjusted train size and created turret scene

---
+++ scene/Train/TrainCart.gd
@@ -0,0 +1,12 @@
+extends KinematicBody2D
+
+func fireTurret():
+	pass
+
+func changeTurret():
+	pass
+	
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
