3ee8785fe8ba1908b93c361cb02f6830bd35872b
N D <klokaktus@gmx.de>
2022-08-17 16:07:11 +0200

    added nonfunctioning signal chain that transfers the bullet hit signal friom bullet up the scene tree

--- scene/Train/Turrets/Turrets.gd
+++ scene/Train/Turrets/Turrets.gd
@@ -1,12 +1,16 @@
 extends Node2D
+
+signal shot_mouse
+
 export (PackedScene) var Bullet
+
 
 func _physics_process(delta):
 	turn()
 	if Input.is_action_just_pressed("shoot"):
 		shoot()
 
-	
+
 func turn():
 	get_node("Barrel").look_at(get_global_mouse_position())
 	
@@ -14,5 +18,9 @@ func turn():
 
 func shoot():
 	var b = Bullet.instance()
+	Bullet.connect("hit_mouse", self, "_on_Bullet_hit_mouse")
 	add_child(b)
 	b.transform = get_node("Barrel").transform
+
+func _on_Bullet_hit_mouse():
+	emit_signal("shot_mouse")
